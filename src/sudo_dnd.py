e7d16dba51337141a0ff3d298b327dccb1ed3ad9
leirdan <uouleirdan@gmail.com>
2026-05-26 10:13:53 -0300

    adição da página de classes

--- src/sudo_dnd.py
+++ src/sudo_dnd.py
@@ -6,13 +6,19 @@ from textual.widgets import Footer, Header, TabbedContent, TabPane, MarkdownView
 START_MARKDOWN = """
 # SUDO DND
 
-O Sudo DND é uma iniciativa de intersecção entre os nerds de RPG e os nerds da computação.
+Sudo DND is an initiative that brings together RPG nerds and computer nerds. Browse the pages to explore the content.
 """
 
 RACES_MARKDOWN = """
 # RACES 
 
-Here, you will know the main characteristics of each race present in Dungeons & Dragons.
+Come explore all the diversity D&D has to offer on this page!
+"""
+
+CLASSES_MARKDOWN = """
+# CLASSES
+
+Come see all the amazing possibilities you can become in D&D.
 """
 
 URL_PREFIX = "https://www.dnd5eapi.co"
@@ -72,12 +78,93 @@ async def fetch_races() -> str:
     return markdown
 
 
+# TODO: vai ficar muito complexo computacionalmente se formos puxar as features
+# async def fetch_features(feats: list) -> str:
+#     pass
+
+
+async def fetch_class_levels(c) -> str:
+    url = URL_PREFIX + c
+    markdown: str = ""
+    headers = {"Accept": "application/json"}
+
+    async with AsyncClient(headers=headers) as client:
+        response = await client.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            for element in data:
+                markdown += f"##### Level {element["level"]}\n"
+                markdown += f"Proficience bonus: (+{element["prof_bonus"]})\n"
+                markdown += f"Features: "
+                for f in element["features"]:
+                    markdown += f"{f["name"]}; "
+                markdown += "\n"
+    return markdown
+
+
+async def fetch_subclasses(subclasses_url: list) -> str:
+    markdown: str = ""
+    headers = {"Accept": "application/json"}
+    async with AsyncClient(headers=headers) as client:
+        requests = [client.get(URL_PREFIX + sub) for sub in subclasses_url]
+        responses = await asyncio.gather(*requests)
+        for response in responses:
+            if response.status_code == 200:
+                data = response.json()
+                markdown += f"##### {data["name"]}\n"
+                markdown += f"Kind: {data["subclass_flavor"]}\n"
+                markdown += f"{data["desc"]}\n"
+    return markdown
+
+
+async def fetch_classes() -> str:
+    base_url = URL_PREFIX + "/api/2014/classes/"
+    classes: list = [
+        "barbarian",
+        "bard",
+        "cleric",
+        "druid",
+        "fighter",
+        "monk",
+        "paladin",
+        "ranger",
+        "rogue",
+        "sorcerer",
+        "warlock",
+        "wizard",
+    ]
+    markdown: str = CLASSES_MARKDOWN
+
+    headers = {"Accept": "application/json"}
+
+    async with AsyncClient(headers=headers) as client:
+        requests = [client.get(base_url + c) for c in classes]
+        responses = await asyncio.gather(*requests)
+        for response in responses:
+            if response.status_code == 200:
+                data = response.json()
+                markdown += f"## {data['name']}\n"
+                markdown += f"### General Stats\n"
+                markdown += f"- Hit Dice: d{data['hit_die']}\n"
+                if data["class_levels"]:
+                    markdown += f"#### Levels\n"
+                    markdown += await fetch_class_levels(data["class_levels"])
+
+                if data["subclasses"]:
+                    markdown += f"### Subclasses\n"
+                    markdown += await fetch_subclasses(
+                        [sub["url"] for sub in data["subclasses"]]
+                    )
+
+    return markdown
+
+
 class SudoDNDApp(App):
     BINDINGS = [
         ("d", "toggle_dark", "Toggle dark mode"),
-        ("1", "show_tab('start_tab')", "Start"),
-        ("2", "show_tab('races_tab')", "Races"),
-        ("3", "show_tab('classes_tab')", "Classes"),
+        ("s", "show_tab('start_tab')", "Start"),
+        ("r", "show_tab('races_tab')", "Races"),
+        ("c", "show_tab('classes_tab')", "Classes"),
     ]
 
     def compose(self) -> ComposeResult:
@@ -88,7 +175,7 @@ class SudoDNDApp(App):
             with TabPane("Races", id="races_tab"):
                 pass
             with TabPane("Classes", id="classes_tab"):
-                yield MarkdownViewer("# Hello! \n ## Olá!", show_table_of_contents=True)
+                pass
         yield Footer()
 
     def action_toggle_dark(self) -> None:
@@ -102,5 +189,10 @@ class SudoDNDApp(App):
     async def on_mount(self) -> None:
         races_data = await fetch_races()
         races_tab = self.query_one("#races_tab", TabPane)
-        markdown_viewer = MarkdownViewer(races_data)
+        markdown_viewer = MarkdownViewer(races_data, show_table_of_contents=False)
         await races_tab.mount(markdown_viewer)
+
+        classes_data = await fetch_classes()
+        classes_tab = self.query_one("#classes_tab", TabPane)
+        markdown_viewer = MarkdownViewer(classes_data, show_table_of_contents=False)
+        await classes_tab.mount(markdown_viewer)
